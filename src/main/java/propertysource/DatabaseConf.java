e4a15e35c1aebfe2becf3a0e1df59132fc5b1b4c
semihokanpehlivan <semihokanp@gmail.com>
2015-04-12 20:10:52 +0300

    org.springframework.core.env.Environment classı ve enjektasyonu

--- src/main/java/propertysource/DatabaseConf.java
+++ src/main/java/propertysource/DatabaseConf.java
@@ -1,11 +1,13 @@
 package propertysource;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by semih on 12.04.2015.
@@ -28,6 +30,8 @@ public class DatabaseConf{
     @Bean
     public Database getDatabase(){
         return new Database(url);
+        // Environment classı ile getProperty("datasource.url"); diyerekte kullanabiliriz. O zaman @Value ye gerek kalmaz
+        // PropertySourcesPlaceholderConfigurer classı o zaman bunun içine tüm enjektasyonunu yapar
     }
 
     // PropertySourcesPlaceholderConfigurer burada ile calistirilan beandir ve görevi ise yukarıdaki ${datasource.url} e
