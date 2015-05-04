917ef3e566ce18211940624289000835fd3e08bd
semihokanpehlivan <semihokanp@gmail.com>
2015-05-05 01:37:33 +0300

    RedirectView ile parametrelerin external direct requestine iletilmesi

---
+++ springmvc/src/main/java/basicspringcontroller/InternalAndExternalDirect.java
@@ -0,0 +1,37 @@
+package basicspringcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ * Created by semih on 05.05.2015.
+ */
+@Controller
+@RequestMapping("/do")
+public class InternalAndExternalDirect {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String doSomething(final ModelMap modelMap) {
+        //burada ise önemli keyword redirect bu sayede do/done şeklinde bir request atabiliriz
+        //RedirectView kullanılarakta bu işlem yapılmaktadır,Bu sınıf sayesinde redirect yaparken parametreleride
+        //redirect edilen yerde kullanabiliriz
+
+
+        return "redirect:do/done";
+    }
+
+    //for http://localhost:8080/do/done
+    @RequestMapping(value = "/done" , method = RequestMethod.GET)
+    public String done(ModelMap modelMap , @RequestParam String name) {
+        modelMap.addAttribute("done", "Doneeeee !!");
+        modelMap.addAttribute("name", name);
+        //return ile bir yönlendirme işlemi yaptık bu bir done.jsp sayfasına işaret etmektedir.
+        //burada yapılan işleme internal direct denilmektedir
+        return "done";
+    }
+
+}
---
+++ springmvc/src/main/webapp/WEB-INF/page/done.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: semih
+  Date: 05.05.2015
+  Time: 01:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>DONE</title>
+</head>
+<body>
+<p>${done}</p>
+<p>user name: ${name}</p>
+</body>
+</html>
